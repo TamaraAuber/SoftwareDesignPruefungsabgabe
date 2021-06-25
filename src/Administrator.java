a9f58070696af39614ef73cf4bc189bcf343cd12
TaJaAu <tamara.janine.auber@hs-furtwangen.de>
2021-06-25 11:02:38 +0200

    Administrator Klasse erstellt

---
+++ src/Administrator.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Administrator {
+
+    private String username = "Batman";
+    private String password = "NaNaNa!";
+
+    public Administrator() {
+
+    }
+
+    //ToDo: Bug wenn im Benutzernamen Leerzeichen sind
+    public void logIn() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Benutzername: ");
+        String enteredUsername = scan.next();
+
+        System.out.print("Passwort: ");
+        String enteredPassword = scan.next();
+
+        if (validateLogInData(enteredUsername, enteredPassword)) {
+            System.out.println("Welcome to the Batcave Mr. Wayne");
+            adminOptions();
+        }
+        else{
+            System.out.println("Activated Selfdestruction. Try again");
+            logIn();
+        }
+
+        scan.close();
+    }
+
+    private boolean validateLogInData(String _username, String _password) {
+        if (username.equals(_username) && password.equals(_password)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void adminOptions() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Wählen sie ihren nächsten Schritt");
+
+        System.out.println("1) Übersicht Termine");
+        System.out.println("2) neuen Termin anlegen");
+        System.out.println("3) Statistik ansehen");
+
+        int selectedOption = scan.nextInt();
+
+        System.err.println("OOOOOOOOOOOOOption: " + selectedOption);
+
+        scan.close();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
-public class Main{
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-      }
+        welcomeUser();
+        userOrAdmin();
+    }
+
+    public static void welcomeUser() {
+        System.out.println("Henlo bei der superduper besten VaccinationAppointmenApp :-)");
+    }
+
+    public static void userOrAdmin() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Termin buchen oder als Administrator fortfahren?");
+        System.out.println("Wählen sie dazu eine der Nummern aus");
+        System.out.println("1) Termin buchen");
+        System.out.println("2) als Administrator anmelden");
+
+        int selectedOption = scan.nextInt();
+
+        switch (selectedOption) {
+            case 1:
+                System.out.println("OptionTerminBuchen");
+                break;
+            case 2:
+                System.out.println("OptionLogIn");
+                Administrator Admin = new Administrator();
+                Admin.logIn();
+                break;
+        }
+
+        scan.close();
+    }
 }
